58e9372109121a7a781132068c0652b676b2c485
ErickCD <isceclair@gmmail.com>
2025-02-17 00:23:48 -0600

    * Se agrega formulario reactivo. * Se agregan funciones CRUD al servicio. * Se enlaza el formulario reactivo con la vista

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,28 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { enviroments } from '../../../environments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
   private baseURL: string = enviroments.baseURL;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getHeroes(): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseURL}/heroes`);
   }
 
-  getHeroById(id: string): Observable<Hero | undefined>{
+  getHeroById(id: string): Observable<Hero | undefined> {
     return this.httpClient
-    .get<Hero>(`${this.baseURL}/heroes/${id}`)
-    .pipe(
-      catchError((error) => of(undefined))
+      .get<Hero>(`${this.baseURL}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
+  }
+
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(
+      `${this.baseURL}/heroes?q=${query}&_limit=6`
     );
   }
 
-  getSuggestions(query: string): Observable<Hero[]>{
-    return this.httpClient.get<Hero[]>(`${this.baseURL}/heroes?q=${query}&_limit=6`);
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseURL}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) {
+      throw Error('The hero must have an id');
+    }
+
+    return this.httpClient.patch<Hero>(
+      `${this.baseURL}/heroes${hero.id}`,
+      hero
+    );
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    if (!id) {
+      throw Error('The id is required');
+    }
+
+    return this.httpClient.delete(`${this.baseURL}/heroes${id}`).pipe(
+      catchError((error) => of(false)), // Si da error, entonces es false
+      map((resp) => true) // Si llega al map, entonces todo ok.
+    );
   }
 }
